cddf080bcac0ce2174f3f3c2cd5abc084f03f051
Samuel <samuelsantxxi@gmail.com>
2021-11-03 23:12:15 -0300

    add root route

--- backend/src/shared/infra/http/routes/index.ts
+++ backend/src/shared/infra/http/routes/index.ts
@@ -4,6 +4,10 @@ import { usersRoutes } from './users.routes';
 
 const router = Router();
 
+router.use('/', (request, response) => {
+  response.send('REST')
+});
 router.use('/users', usersRoutes);
+
 
 export { router };
